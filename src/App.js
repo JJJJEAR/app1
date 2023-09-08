29abfaf74690286d152d0f82faa328a2d9d6ac37
JJJJEAR <110074209+JJJJEAR@users.noreply.github.com>
2023-09-08 15:36:53 +0700

    Add Context-header2 and context-content2
    Add Context-header2 and context-content2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import Header from './context-header'
 import { userContext } from './context'
+import Content from './context-content'
+import Header2 from "./context-header2";
+import Content2 from "./content-content2";
 
 export default function App() {
+  let [user,setUser] = React.useState()
   return (
     <>
     <div> 
-      <userContext.Provider value={'tom Jerry'}>
-        <Header/>
+      <userContext.Provider value={[user,setUser]}>
+        <Header2/>
+        <Content2/>
       </userContext.Provider>
        
     </div>
