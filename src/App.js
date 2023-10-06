be5a52496ae63c12264493a28cf1e801ace872d4
JJJJEAR <110074209+JJJJEAR@users.noreply.github.com>
2023-10-06 14:45:47 +0700

    add form-validation.js
    add form-validation.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import FormText from "./form-text";
 import FormCheck from "./form-check";
+import FormValidation from "./form-validation";
 
 export default function App() {
   // let [user,setUser] = React.useState()
   return (
     <>
     <div> 
-    <FormText/>
-    <FormCheck/>
-
+    {/* <FormText/>
+    <FormCheck/> */}
+    <FormValidation/>
     </div>
     </>
   );
